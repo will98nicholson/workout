7b7ca9ff9b7c13a01d1aea0d05366d3148e4db53
William Nicholson <will98nicholson@gmail.com>
2021-05-19 14:14:01 -0500

    workout model fixed

--- models/Workout.js
+++ models/Workout.js
@@ -2,19 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: new Date()
-
-    },
+    day: { type: Date, default: new Date() },
     type: { type: String, required: true },
     name: { type: String, required: true },
-    duration: { type: Number, required: true },
     distance: { type: Number, required: true },
     weight: { type: Number, required: true },
-    reps: { type: Number, required: true },
     sets: { type: Number, required: true },
+    reps: { type: Number, required: true },
+    duration: { type: Number, required: true },
+
 });
-const Workout = mongoose.model('workout', workoutSchema);
+const Workout = mongoose.model('Workout', workoutSchema);
 
 module.exports = Workout;
